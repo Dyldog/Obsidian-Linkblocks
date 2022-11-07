8d663bf7aec1dec6d654580b2dc3786d1c6dc6af
Dylan Elliott <dylan.elliott@arq.group>
2022-11-07 15:45:33 +1100

    Add release it requirements

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+Sections are used under each version as follows:
+- `Added` for new features.
+- `Changed` for changes in existing functionality.
+- `Deprecated` for soon-to-be removed features.
+- `Removed` for now removed features.
+- `Fixed` for any bug fixes.
+- `Security` in case of vulnerabilities.
+
+## [Unreleased]
+
+### Added
+
+- Nothing so far
+
+## [1.0.2] - 2022-11-07
+
+### Added
+
+- Initial Release
--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,65 @@
-
 import { Plugin } from "obsidian";
 import moment from "moment";
 
 // Taken from https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
 
 function cyrb128(str) {
-    let h1 = 1779033703, h2 = 3144134277,
-        h3 = 1013904242, h4 = 2773480762;
-    for (let i = 0, k; i < str.length; i++) {
-        k = str.charCodeAt(i);
-        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
-        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
-        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
-        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
-    }
-    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
-    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
-    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
-    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
-    return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
+  let h1 = 1779033703,
+    h2 = 3144134277,
+    h3 = 1013904242,
+    h4 = 2773480762;
+  for (let i = 0, k; i < str.length; i++) {
+    k = str.charCodeAt(i);
+    h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+    h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+    h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+    h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+  }
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+  return [
+    (h1 ^ h2 ^ h3 ^ h4) >>> 0,
+    (h2 ^ h1) >>> 0,
+    (h3 ^ h1) >>> 0,
+    (h4 ^ h1) >>> 0,
+  ];
 }
 
 function sfc32(a, b, c, d) {
-    return function() {
-      a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
-      var t = (a + b) | 0;
-      a = b ^ b >>> 9;
-      b = c + (c << 3) | 0;
-      c = (c << 21 | c >>> 11);
-      d = d + 1 | 0;
-      t = t + d | 0;
-      c = c + t | 0;
-      return (t >>> 0) / 4294967296;
-    }
+  return function () {
+    a >>>= 0;
+    b >>>= 0;
+    c >>>= 0;
+    d >>>= 0;
+    let t = (a + b) | 0;
+    a = b ^ (b >>> 9);
+    b = (c + (c << 3)) | 0;
+    c = (c << 21) | (c >>> 11);
+    d = (d + 1) | 0;
+    t = (t + d) | 0;
+    c = (c + t) | 0;
+    return (t >>> 0) / 4294967296;
+  };
 }
 
 function mulberry32(a) {
-    return function() {
-      var t = a += 0x6D2B79F5;
-      t = Math.imul(t ^ t >>> 15, t | 1);
-      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-      return ((t ^ t >>> 14) >>> 0) / 4294967296;
-    }
+  return function () {
+    let t = (a += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  };
 }
 
 function getRandomiser(seed) {
-    // Create cyrb128 state:
-  var seed = cyrb128(seed);
+  // Create cyrb128 state:
+  const seed = cyrb128(seed);
   // Four 32-bit component hashes provide the seed for sfc32.
-  var rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
+  let rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
   // Only one 32-bit component hash is needed for mulberry32.
-  var rand = mulberry32(seed[0]);
+  let rand = mulberry32(seed[0]);
 
   return rand;
 }
@@ -72,8 +81,8 @@ const colors = [
   "white",
   "silver",
   "gray",
-  "black"
-]
+  "black",
+];
 function randomColor(randomiser) {
   console.log(randomiser());
   return colors[Math.round(randomiser() * colors.length)];
@@ -81,11 +90,11 @@ function randomColor(randomiser) {
 
 function isValidHttpUrl(string) {
   let url;
-  
+
   try {
     url = new URL(string);
   } catch (_) {
-    return false;  
+    return false;
   }
 
   return url.protocol === "http:" || url.protocol === "https:";
@@ -98,11 +107,11 @@ function makeDate(string) {
 }
 
 function getPart(parts, index) {
-  var part = parts[index];
+  const part = parts[index];
 
-  var match = [...part.matchAll(/\$DATE\(([A-Za-z-]+)/g)];
-  console.log(match)
-  
+  const match = [...part.matchAll(/\$DATE\(([A-Za-z-]+)/g)];
+  console.log(match);
+
   if (match.length == 0) {
     console.log("Returning part");
     return part;
@@ -112,61 +121,45 @@ function getPart(parts, index) {
   }
 }
 
-
-
 export default class LinkBlocksPlugin extends Plugin {
   async onload() {
-    this.registerMarkdownCodeBlockProcessor("lblock", (source, el, ctx) => {      
-      var randomiser = getRandomiser(ctx.sourcePath);
+    this.registerMarkdownCodeBlockProcessor("lblock", (source, el, ctx) => {
+      const randomiser = getRandomiser(ctx.sourcePath);
       console.log(ctx);
       const rows = source.split("\n");
 
       if (rows.length > 0) {
-        var box = el.createEl("div", { cls: "linkbox"});
+        const box = el.createEl("div", { cls: "linkbox" });
 
-        rows
-          .map((row) => {
-            // el.createEl("p", text:  row});
+        rows.map((row) => {
+          // el.createEl("p", text:  row});
 
-            if (row.trim().length == 0) {
-              box.createEl("div", { cls: "break" });
-            } else {
-              var parts = row.split("|")
+          if (row.trim().length == 0) {
+            box.createEl("div", { cls: "break" });
+          } else {
+            const parts = row.split("|");
 
-              if (parts.length == 1) {
-                box.createEl("a", { 
-                  text: getPart(parts, 0), 
-                  cls: "internal-link block link " + randomColor(randomiser);
-                });
-              } else if (isValidHttpUrl(parts[1])) {
-                box.createEl("a", { 
-                  text: getPart(parts, 0), 
-                  href: getPart(parts, 1),
-                  cls: "block link " + randomColor(randomiser); 
-                });
-              } else if (parts.length == 2) {
-                box.createEl("a", { 
-                  text: getPart(parts, 0), 
-                  href: getPart(parts, 1),
-                  cls: "internal-link block link " + randomColor(randomiser);
-                });
-              }
+            if (parts.length == 1) {
+              box.createEl("a", {
+                text: getPart(parts, 0),
+                cls: "internal-link block link " + randomColor(randomiser),
+              });
+            } else if (isValidHttpUrl(parts[1])) {
+              box.createEl("a", {
+                text: getPart(parts, 0),
+                href: getPart(parts, 1),
+                cls: "block link " + randomColor(randomiser),
+              });
+            } else if (parts.length == 2) {
+              box.createEl("a", {
+                text: getPart(parts, 0),
+                href: getPart(parts, 1),
+                cls: "internal-link block link " + randomColor(randomiser),
+              });
             }
           }
+        });
       }
-
-      // const table = el.createEl("table");
-      // const body = table.createEl("tbody");
-
-      // for (let i = 0; i < rows.length; i++) {
-      //   const cols = rows[i].split(",");
-
-      //   const row = body.createEl("tr");
-
-      //   for (let j = 0; j < cols.length; j++) {
-      //     row.createEl("td", { text: cols[j] });
-      //   }
-      // }
     });
   }
 }
